ac5d799d3f993b55e781f2c6f59d7f6edab6751b
Uwe <usx303@googlemail.com>
2018-11-02 14:24:49 +0100

    added MIT license header to files

--- src/bb-blue-battery-state.cpp
+++ src/bb-blue-battery-state.cpp
@@ -5,10 +5,31 @@
  *            based on the examples and librobotcontrol
  *						by James Strawson
  *
- * @license   MIT
- *
  * @author    usxbrix
  * @date      Nov 2018
+ *
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2018 usxbrix
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
@@ -19,7 +40,7 @@ class BatteryState
 {
 protected:
   // Subscriber to thrusters use in percent
-  
+
 
 public:
   BatteryState(ros::NodeHandle &ros_node)
@@ -35,9 +56,9 @@ public:
 
   void calculateBatteryCondition()
   {
-     
+
     battery_msg_.percentage = 100;
- 
+
   }
 
   void Publish()

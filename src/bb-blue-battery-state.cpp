4782a11aad9b91efe7648f976228447211067902
Uwe <usx303@googlemail.com>
2018-11-03 22:39:13 +0100

    various bb-blue-battery-state updates for ROS

--- src/bb-blue-battery-state.cpp
+++ src/bb-blue-battery-state.cpp
@@ -36,7 +36,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/BatteryState.h"
 #include <rc/adc.h>
-#include <stdlib.h> // for atoi() and exit()
+//#include <stdlib.h> // for atoi() and exit()
 
 #define VOLTAGE_DISCONNECT      1 // Threshold for detecting disconnected battery
 
@@ -53,8 +53,9 @@ public:
     battery_state_publisher_ = ros_node.advertise<sensor_msgs::BatteryState>("battery_state", 1);
 
     // battery present and full
-    battery_msg_.percentage = 100;
-    battery_msg_.present = 1;
+    //battery_msg_.percentage = 100;
+    //battery_msg_.present = 1;
+    battery_msg_.power_supply_technology = 3;
 
   }
 
@@ -71,24 +72,36 @@ public:
     jack_voltage = rc_adc_dc_jack();
     // sanity check the SDC didn't return an error
     if(pack_voltage<0.0 || jack_voltage<0.0){
-      fprintf(stderr,"ERROR: can't read voltages\n");
+      ROS_ERROR("ERROR: can't read voltages");
+      //fprintf(stderr,"ERROR: can't read voltages\n");
       return -1;
     }
     // check if a pack is on the 2S balance connector
     if(pack_voltage<VOLTAGE_DISCONNECT){
       pack_voltage = 0;
+      battery_msg_.present = 0;
     }
     if(jack_voltage<VOLTAGE_DISCONNECT){
       jack_voltage = 0;
     }
     // 2S pack, so divide by two for cell voltage
     cell_voltage = pack_voltage/2;
-    // TODO: output results to ROS message
-    printf("\rPack: %0.2lfV   Cell: %0.2lfV   DC Jack: %0.2lfV  ", \
+
+    ROS_INFO("Pack: %0.2lfV   Cell: %0.2lfV   DC Jack: %0.2lfV  ", \
       pack_voltage, cell_voltage, jack_voltage);
 
-    battery_msg_.percentage = 100;
+    //battery_msg_.percentage = 100;
+    battery_msg_.voltage = pack_voltage;
 
+    /** TODO: percentage
+    * https://github.com/StrawsonDesign/librobotcontrol/blob/master/services/rc_battery_monitor/src/rc_battery_monitor.c
+    *
+    * battery_msg_.percentage = 100;
+    */
+
+    /** TODO: power_supply_status,power_supply_health
+    *
+    */
   }
 
   void Publish()
@@ -105,12 +118,24 @@ protected:
 
 int main(int argc, char** argv)
 {
-  if(rc_adc_init()==-1) exit();
+
 
   ros::init(argc, argv, "battery_state");
   ros::NodeHandle ros_node;
 
+  ROS_INFO("Initializing node %s in namespace: %s", ros::this_node::getName().c_str(), ros::this_node::getNamespace().c_str() );
+
+  // TODO: use parameters
+  int pst; //power_supply_technology
+  ros_node.param("~power_supply_technology", pst, 3);
+
   ros::Rate loop_rate(1); // 1 hz
+
+  if(rc_adc_init()==-1)
+  {
+     ROS_ERROR("Initialize ADC: FAILED");
+     return -1;
+  }
 
   BatteryState batteryState(ros_node);
 
